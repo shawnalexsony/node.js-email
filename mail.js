19dc353d0f22d5e56eabed01d91b0440fbac5c06
Shawn Alex Sony <shawnalex97@gmail.com>
2019-01-09 15:03:16 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -5,14 +5,14 @@ var smtpConfig = {
   port: 465,
   secure: true, // use SSL
   auth: {
-      user: 'shawnal.bot@gmail.com',
-      pass: '1.2.3.a.s.d'
+      user: 'gmail address',
+      pass: 'password'
   }
 };
 var transporter = nodemailer.createTransport(smtpConfig);
 var mailOptions = {
-  from: 'shawnal.bot@gmx.com',
-  to: 'shawnalex97@gmail.com',
+  from: 'sender gmail address ',
+  to: 'recipient address',
   subject: 'Sending Email using Node.js',
   text: 'Success!!!'
 };
